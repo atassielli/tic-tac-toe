9d5ed7b067fe4d234d90aab24447ac7c5391b898
atassielli <anthonytassielli@gmail.com>
2022-08-22 20:50:31 -0700

    Build form for entry of player names and symbol selection
    form validates player names and symbol selection

--- script.js
+++ script.js
@@ -1,29 +1,59 @@
 const gameBoard = (() => {
     const board = { 
-        entries: ['x', 'x'],
-        year: 5
+        entries: [],
     };
     return board.entries
 })();
 
-const page = document.querySelector('.container')
-
 const displayController = (() => {
-    const display = document.createElement('div');
-    display.textContent = `'${gameBoard}'`
-    display.classList.add('test')
-    page.appendChild(display);
 })();
 
-//const Player = (label, symbol) => {
-//    const getName // write an html input for getting the players name and have it map to name 
- //   const getSymbol // write an html input for getting the player symbol and have it map to symbol 
+//Player Creation
+
+const Player = (name, symbol) => {
+    const getName = () => name;
+    const getSymbol = () => symbol;
+    return {getName, getSymbol}
+}
+
+let beginGame = document.querySelector('.beginGame');
+beginGame.addEventListener('click', () => {
+    console.log(getPlayerOneName.value)
+    console.log(getPlayerTwoName.value)
+    validateNames();
+    valdiateSymbol();
+})
+
+
+function makePlayer () {
+    let player1 = Player(`'${getPlayerOneName}'`, `${getSymbolPlayer1()}`);
+    let player2 = Player(`${getPlayerTwoName}`, `${getSymbolPlayer2()}`);
+}
+
 
 // Validations
 
- let getPlayerOneName = document.querySelector('.playerOneName');
+ const getPlayerOneName = document.querySelector('.playerOneName');
  let getPlayerTwoName = document.querySelector('.playerTwoName');
- let options = document.getElementsByClassName('.symbol');
  let symbolX = document.querySelector('.x');
  let symbolO = document.querySelector('.o');
+ let getPlayerOneSymbol;
 
+ function validateNames() {
+     if ((getPlayerOneName.value.length === 0) || (getPlayerTwoName.value.length === 0)) {
+         alert('Please fill out player names before proceeding to the game');
+     } else {
+         return
+     }
+ }
+
+ function valdiateSymbol() {
+    if(document.querySelector('.x').checked) {
+         return 'x';
+    } else if (document.querySelector('.o').checked) {
+         return 'o'
+    } else {
+        alert('Please select a symbol before proceeding to the game')
+        return
+    }
+ }
