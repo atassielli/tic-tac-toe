9ed4c03154faa5b01ca2d52948e65ac99301bfd9
atassielli <anthonytassielli@gmail.com>
2022-08-24 20:18:31 -0700

    Use factory function to create player profiles
    form data is passed through factory to create player profiles

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//Game Controls
+
 const gameBoard = (() => {
     const board = { 
         entries: [],
@@ -8,26 +10,27 @@ const gameBoard = (() => {
 const displayController = (() => {
 })();
 
-//Player Creation
-
-const Player = (name, symbol) => {
-    const getName = () => name;
-    const getSymbol = () => symbol;
-    return {getName, getSymbol}
-}
-
 let beginGame = document.querySelector('.beginGame');
 beginGame.addEventListener('click', () => {
     console.log(getPlayerOneName.value)
     console.log(getPlayerTwoName.value)
     validateNames();
-    valdiateSymbol();
+    valdiatePlayer1Symbol();
+    valdiatePlayer2Symbol();
+    makePlayer();
 })
 
+//Player Creation
+
+const Player = (name, symbol) => {
+    const getName = name;
+    const getSymbol = symbol;
+    return {getName, getSymbol}
+}
 
 function makePlayer () {
-    let player1 = Player(`'${getPlayerOneName}'`, `${getSymbolPlayer1()}`);
-    let player2 = Player(`${getPlayerTwoName}`, `${getSymbolPlayer2()}`);
+    let player1 = Player(getPlayerOneName.value, valdiatePlayer1Symbol());
+    let player2 = Player(getPlayerTwoName.value, valdiatePlayer2Symbol());
 }
 
 
@@ -37,7 +40,6 @@ function makePlayer () {
  let getPlayerTwoName = document.querySelector('.playerTwoName');
  let symbolX = document.querySelector('.x');
  let symbolO = document.querySelector('.o');
- let getPlayerOneSymbol;
 
  function validateNames() {
      if ((getPlayerOneName.value.length === 0) || (getPlayerTwoName.value.length === 0)) {
@@ -47,13 +49,27 @@ function makePlayer () {
      }
  }
 
- function valdiateSymbol() {
+ function valdiatePlayer1Symbol() {
     if(document.querySelector('.x').checked) {
-         return 'x';
+         return 'X';
     } else if (document.querySelector('.o').checked) {
-         return 'o'
+         return 'O'
     } else {
         alert('Please select a symbol before proceeding to the game')
         return
     }
  }
+
+ function valdiatePlayer2Symbol() {
+    if(document.querySelector('.x').checked) {
+         return 'O';
+    } else if (document.querySelector('.o').checked) {
+         return 'X'
+    }
+ }
+
+
+ //when you click begin game it needs to hide the form and bring up the board game
+ //Change the instruction line to read the players name and state that it is their turn
+ //When a square is clicked it takes the current players symbol and puts it on the square
+ //Possibly make each square into an object that holds a clicked property and a symbol property, this could run the game to not allow players to click a spot that has already been taken 
